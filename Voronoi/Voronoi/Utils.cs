f354050cddad9764d5c0cd45184ed38122bd82c9
Corey O'Neil <lessthanthree@gmail.com>
2016-08-14 15:41:51 -0500

    Spot for frequently used operations on collections

---
+++ Voronoi/Voronoi/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using coneil.Math.Voronoi.Geometry;
+
+namespace coneil.Math.Voronoi
+{
+    internal sealed static class Utils
+    {
+        public static List<LineSegment> GetDelaunayLinesForEdges(List<Edge> edges)
+        {
+            return edges.Select(x => x.DelaunayLine).ToList();
+        }
+
+        public static List<Edge> SelectEdgesForSitePoint(Point coord, List<Edge> edgesToTest)
+        {
+            return edgesToTest.Where(x => ((x.LeftSite != null && x.LeftSite.Coord.Equals(coord)) || (x.RightSite != null && x.RightSite.Coord.Equals(coord)))).ToList();
+        }
+
+        public List<LineSegment> GetVisibleLineSegments(List<Edge> edges)
+        {
+            return edges.Where(x => x.Visible).Select(x => new LineSegment(x.ClippedVertices[LR.LEFT], x.ClippedVertices[LR.RIGHT])).ToList();
+        }
+    }
+}
